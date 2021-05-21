e3b6f86fc83f77a12df8c4d97df90a6f3ebd8803
odupree1988 <odupree1988@gmail.com>
2021-05-21 17:30:01 -0400

    UX enhancements

--- assets/js/script.js
+++ assets/js/script.js
@@ -130,10 +130,20 @@ $(".card .list-group").sortable({
   scroll: false,
   tolerance: "pointer",
   helper: "clone",
-  activate: function (event) {},
-  deactivate: function (event) {},
-  over: function (event) {},
-  out: function (event) {},
+  activate: function (event, ui) {
+    $(this).addClass("dropover");
+    $(".bottom-trash").addClass("bottom-trash-drag");
+  },
+  deactivate: function (event, ui) {
+    $(this).removeClass("dropover");
+    $(".bottom-trash").removeClass("bottom-trash-drag");
+  },
+  over: function (event) {
+    $(event.target).addClass("dropover-active");
+  },
+  out: function (event) {
+    $(event.target).removeClass("dropover-active");
+  },
   update: function (event) {
     //array to store the task data in
     var tempArr = [];
@@ -158,7 +168,7 @@ $(".card .list-group").sortable({
     //update array on tasks object and save
     tasks[arrName] = tempArr;
     saveTasks();
-    console.log(tempArr);
+    // console.log(tempArr);
   },
 });
 
@@ -167,13 +177,13 @@ $("#trash").droppable({
   tolerance: "touch",
   drop: function (event, ui) {
     ui.draggable.remove();
-    console.log("drop");
+    // console.log("drop");
   },
   over: function (event, ui) {
-    console.log("over");
+    $(".bottom-trash").addClass("bottom-trash-active");
   },
   out: function (event, ui) {
-    console.log("out");
+    $(".bottom-trash").removeClass("bottom-trash-active");
   },
 });
 
@@ -190,7 +200,7 @@ $("#task-form-modal").on("shown.bs.modal", function () {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function () {
+$("#task-form-modal .btn-save").click(function () {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
@@ -212,15 +222,16 @@ $("#task-form-modal .btn-primary").click(function () {
 });
 
 var auditTask = function (taskEl) {
+  console.log(taskEl);
   //get date from task element
   var date = $(taskEl).find("span").text().trim();
   //ensure it worked
-  console.log(date);
+  // console.log(date);
 
   //convert to moment object at 5:00pm
   var time = moment(date, "L").set("hour", 17);
   //this should print out an object for the value of the date variable, but at 5:00pm of that date
-  console.log(time);
+  // console.log(time);
 
   //remove any old classes from element
   $(taskEl).removeClass("list-group-item-warning list-group-item-danger");
@@ -248,3 +259,9 @@ $("#remove-tasks").on("click", function () {
 
 // load tasks for the first time
 loadTasks();
+
+setInterval(function () {
+  $(".card .list-group-item").each(function (index, el) {
+    auditTask(el);
+  });
+}, 1000 * 60 * 30);
